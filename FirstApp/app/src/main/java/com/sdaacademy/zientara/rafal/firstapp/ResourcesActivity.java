7aa4a9eb50554ebaaa84394651bca67c7c201e66
Rafal Zientara <rafal.zientara92@gmail.com>
2017-06-05 20:49:14 +0200

    final commit

--- FirstApp/app/src/main/java/com/sdaacademy/zientara/rafal/firstapp/ResourcesActivity.java
+++ FirstApp/app/src/main/java/com/sdaacademy/zientara/rafal/firstapp/ResourcesActivity.java
@@ -2,12 +2,22 @@ package com.sdaacademy.zientara.rafal.firstapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class ResourcesActivity extends AppCompatActivity {
+
+    private ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_resources);
+
+        imageView = (ImageView) findViewById(R.id.resources_imageView);
+    }
+
+    public void setRandomPicture(View view) {
+        imageView.setImageResource(R.drawable.ic_pregnant_woman_black_24dp);
     }
 }
