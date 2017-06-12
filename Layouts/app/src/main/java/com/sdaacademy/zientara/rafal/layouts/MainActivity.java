f99988960c92923533e2f7ae6360e007a0f1af54
Rafal Zientara <rafal.zientara92@gmail.com>
2017-06-12 17:38:59 +0200

    chat simulator

--- Layouts/app/src/main/java/com/sdaacademy/zientara/rafal/layouts/MainActivity.java
+++ Layouts/app/src/main/java/com/sdaacademy/zientara/rafal/layouts/MainActivity.java
@@ -2,14 +2,16 @@ package com.sdaacademy.zientara.rafal.layouts;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
-    
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
     private EditText messageEditText;
     private ImageView sendImage;
     private LinearLayout messagesLinearLayout;
@@ -23,7 +25,26 @@ public class MainActivity extends AppCompatActivity {
         sendImage = (ImageView)findViewById(R.id.chat_sendImage);
         messagesLinearLayout = (LinearLayout)findViewById(R.id.chat_messagesLinearLayout);
 
-        Toast.makeText(this, "Wpisujemy tekst" ,Toast.LENGTH_LONG).show();
+        sendImage.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        sendMessage(v);
+    }
+
+    public void sendMessage(View v) {
+        String message = messageEditText.getText().toString();
+        messageEditText.setText(null);
+
+        Toast.makeText(this, message, Toast.LENGTH_LONG).show();
+
+
+        LayoutInflater inflater = getLayoutInflater();
+        TextView myMessageTextView = (TextView) inflater.inflate(R.layout.my_message_item, null, false);
+        myMessageTextView.setText(message);
+
+        messagesLinearLayout.addView(myMessageTextView);
 
     }
 }
