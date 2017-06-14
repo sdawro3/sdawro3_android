b42c49bc06f5446cd1d00acef498d420435bb87a
Rafal Zientara <rafal.zientara92@gmail.com>
2017-06-14 18:57:03 +0200

    completed random list

--- RotateList/app/src/main/java/com/sdaacademy/zientara/rafal/rotatelist/MainActivity.java
+++ RotateList/app/src/main/java/com/sdaacademy/zientara/rafal/rotatelist/MainActivity.java
@@ -1,29 +1,66 @@
 package com.sdaacademy.zientara.rafal.rotatelist;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListAdapter;
 import android.widget.ListView;
 
 import java.util.ArrayList;
-import java.util.List;
+import java.util.Random;
 
 public class MainActivity extends BaseActivity {
+
+    private ListView listView;
+    private String[] androidVersions;
+    private ArrayList<String> stringList;
+    private Random random;
+    private ArrayAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        listView = (ListView) findViewById(R.id.listView);
+        androidVersions = getResources().getStringArray(R.array.wersje_androida);
+        stringList = new ArrayList<>();
+        random = new Random();
+        initAdapter();
+    }
 
-        ListView listView = (ListView) findViewById(R.id.listView);
-
-        String[] stringArray = getResources().getStringArray(R.array.wersje_androida);
-        //String[] stringList = new String[] {"jeden", "dwa"};
-        List<String> stringList = new ArrayList<>();//new String[] {"jeden", "dwa"};
-
-        ListAdapter adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, stringList);
-
+    private void initAdapter() {
+        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, stringList);
         listView.setAdapter(adapter);
+    }
+
+    public void randomVersion(View view) {
+        String randomString = getRandomVersion();
+        stringList.add(randomString);
+        refreshList();
+    }
+
+    private void refreshList() {
+        adapter.notifyDataSetChanged();
+    }
+
+    private String getRandomVersion() {
+        int randomIndex = random.nextInt(androidVersions.length);
+        return androidVersions[randomIndex];
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putStringArrayList("lista", stringList);
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+
+        ArrayList<String> savedList = savedInstanceState.getStringArrayList("lista");
+        if (savedList != null)
+            stringList.addAll(savedList);
+        refreshList();
     }
 }
